86d399994beb27697487c996b3d02507a229b890
kotleni <yavarenikya@gmail.com>
2023-09-23 20:05:18 +0300

    hot-fix: memory leak in gl buffers binding

--- src/renderer.rs
+++ src/renderer.rs
@@ -7,6 +7,11 @@ pub struct RatRenderer {
     pub vao: GLuint,
     pub vbo: GLuint,
     pub ebo: GLuint,
+
+    index_buf: GLuint,
+    pos_buf: GLuint,
+    norm_buf: GLuint,
+    uv_buf: GLuint,
 }
 
 impl RatRenderer {
@@ -15,6 +20,11 @@ impl RatRenderer {
             vao: 0,
             vbo: 0,
             ebo: 0,
+
+            index_buf: 0,
+            pos_buf: 0,
+            norm_buf: 0,
+            uv_buf: 0,
         }
     }
 
@@ -29,13 +39,8 @@ impl RatRenderer {
         let mesh = &model.mesh;
 
         unsafe {
-            let mut index_buf: GLuint = 0;
-            let mut pos_buf: GLuint = 0;
-            let mut norm_buf: GLuint = 0;
-            let mut uv_buf: GLuint = 0;
-
-            gl::GenBuffers(1, &mut index_buf);
-            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, index_buf);
+            gl::GenBuffers(1, &mut self.index_buf);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, self.index_buf);
             gl::BufferData(
                 gl::ELEMENT_ARRAY_BUFFER,
                 (mesh.indices.len() * std::mem::size_of::<GLuint>()) as GLsizeiptr,
@@ -43,8 +48,8 @@ impl RatRenderer {
                 gl::STATIC_DRAW,
             );
 
-            gl::GenBuffers(1, &mut pos_buf);
-            gl::BindBuffer(gl::ARRAY_BUFFER, pos_buf);
+            gl::GenBuffers(1, &mut self.pos_buf);
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.pos_buf);
             gl::BufferData(
                 gl::ARRAY_BUFFER,
                 (mesh.positions.len() * std::mem::size_of::<GLfloat>()) as GLsizeiptr,
@@ -52,8 +57,8 @@ impl RatRenderer {
                 gl::STATIC_DRAW,
             );
 
-            gl::GenBuffers(1, &mut norm_buf);
-            gl::BindBuffer(gl::ARRAY_BUFFER, norm_buf);
+            gl::GenBuffers(1, &mut self.norm_buf);
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.norm_buf);
             gl::BufferData(
                 gl::ARRAY_BUFFER,
                 (mesh.normals.len() * std::mem::size_of::<GLfloat>()) as GLsizeiptr,
@@ -61,8 +66,8 @@ impl RatRenderer {
                 gl::STATIC_DRAW,
             );
 
-            gl::GenBuffers(1, &mut uv_buf);
-            gl::BindBuffer(gl::ARRAY_BUFFER, uv_buf);
+            gl::GenBuffers(1, &mut self.uv_buf);
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.uv_buf);
             gl::BufferData(
                 gl::ARRAY_BUFFER,
                 (mesh.texcoords.len() * std::mem::size_of::<GLfloat>()) as GLsizeiptr,
@@ -73,29 +78,46 @@ impl RatRenderer {
             gl::GenVertexArrays(1, &mut self.vao);
             gl::BindVertexArray(self.vao);
 
-            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, index_buf);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, self.index_buf);
 
             // Position
-            gl::BindBuffer(gl::ARRAY_BUFFER, pos_buf);
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.pos_buf);
             gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 0, std::ptr::null());
             gl::EnableVertexAttribArray(0); // Vertex position
 
             // Normal
-            gl::BindBuffer(gl::ARRAY_BUFFER, norm_buf);
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.norm_buf);
             gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, 0, std::ptr::null());
             gl::EnableVertexAttribArray(1); // Normal
 
             // UV
-            gl::BindBuffer(gl::ARRAY_BUFFER, uv_buf);
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.uv_buf);
             gl::VertexAttribPointer(2, 2, gl::FLOAT, gl::FALSE, 0, std::ptr::null());
             gl::EnableVertexAttribArray(2); // UV
 
             gl::BindVertexArray(0);
             gl::BindBuffer(gl::ARRAY_BUFFER, 0);
             gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, 0);
+
+            gl::DeleteBuffers(1, &self.index_buf);
+            gl::DeleteBuffers(1, &self.pos_buf);
+            gl::DeleteBuffers(1, &self.norm_buf);
+            gl::DeleteBuffers(1, &self.uv_buf);
         }
 
         mesh.indices.len() as i32
+    }
+
+    pub fn unbind_buffers(&self) {
+        unsafe {
+            gl::BindVertexArray(0);
+            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, 0);
+
+            gl::DeleteVertexArrays(1, &self.vao);
+            // gl::DeleteVertexArrays(1, &self.vbo);
+            // gl::DeleteVertexArrays(1, &self.ebo);
+        }
     }
 
     pub fn render_model(&mut self, object: &RatObject, camera: &Camera) {
@@ -113,8 +135,9 @@ impl RatRenderer {
                     gl::UNSIGNED_INT,
                     std::ptr::null(),
                 );
-                gl::BindVertexArray(0);
             }
+
+            self.unbind_buffers();
         }
     }
 }
